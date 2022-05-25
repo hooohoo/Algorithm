737f1e1c60388b130ffb7c3edfd3e59ae397002d
hphptop <hphptop@naver.com>
2022-05-25 22:28:54 +0900

    220525 : BubbleSort

---
+++ 220525_BubbleSort/BubbleSort.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+int main() {
+
+	int arr[10] = {5, 8, 2, 1, 6, 3, 7, 4, 9, 0};
+
+	int count = sizeof(arr) / sizeof(int);
+	int check = 0;	// 반복문 실행 수 저장용 
+
+	
+	for (int i = 0; i < count; i++)
+	{
+		int t = 0;
+		for (int j = t+1; j < count; j++)
+		{
+			if (arr[t] > arr[j])
+			{
+				int temp = arr[t];
+				arr[t] = arr[j];
+				arr[j] = temp;
+			}
+			t = j;
+			check++;
+		}
+	}
+	
+	for (int i = 0; i < count; i++)
+	{
+		printf("%d\n", arr[i]);
+	}
+
+	printf("일반 BubbleSort 총 반복 회수 : %d\n\n", check);
+	check = 0;
+	
+
+	//-----------------------------------
+	// 향상된 bubble sort
+
+	int jCount = count;
+	for (int i = 0; i < count; i++)
+	{
+		int t = 0;
+		for (int j = t + 1; j < jCount; j++)
+		{
+			if (arr[t] > arr[j])
+			{
+				int temp = arr[t];
+				arr[t] = arr[j];
+				arr[j] = temp;
+			}
+			t = j;
+			check++;	// 총 반복 수 확인 출력용
+		}
+		jCount--;
+	}
+
+	for (int i = 0; i < count; i++)
+	{
+		printf("%d\n", arr[i]);
+	}
+
+	printf("향상된 Bubble Sort 총 반복 회수 : %d\n\n", check);
+
+
+
+	return 0;
+}
